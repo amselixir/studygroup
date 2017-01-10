378efe980d3e3cf5846d253c857e61a2f6566548
Tonći Galić <tuxified@gmail.com>
2017-01-10 17:52:56 +0100

    hopefully improve error messages

--- studygroup_003/.workshop/exercises/gen_cachex/test/check.exs
+++ studygroup_003/.workshop/exercises/gen_cachex/test/check.exs
@@ -2,27 +2,51 @@ defmodule Workshop.Exercise.GenCachexCheck do
   use Workshop.Validator
 
   verify "store and retrieve value" do
-    {:ok, pid} = GenCachex.start_link()
-    nil == GenCachex.get(pid, "name")
-    :ok == GenCachex.put(pid, "name", "Derek")
-    "Derek" == GenCachex.get(pid, "name") && :ok
+    with {:ok, pid} <- GenCachex.start_link(),
+      nil <- GenCachex.get(pid, "name"),
+      :ok <- GenCachex.put(pid, "name", "Derek"),
+      "Derek" <- GenCachex.get(pid, "name")
+    do
+      :ok
+    else
+      result when is_binary(result) ->
+        {:error, "got #{inspect result} while expected nil"}
+      something ->
+        {:error, "unexpected, got #{something}"}
+    end
   end
 
   verify "store and delete value" do
-    {:ok, pid} = GenCachex.start_link()
-    nil == GenCachex.get(pid, "name")
-    :ok == GenCachex.put(pid, "name", "Thijs")
-    "Thijs" == GenCachex.get(pid, "name")
-    :ok == GenCachex.delete(pid, "name")
-    nil == GenCachex.get(pid, "name") && :ok
+    with {:ok, pid} <- GenCachex.start_link(),
+      nil <- GenCachex.get(pid, "name"),
+      :ok <- GenCachex.put(pid, "name", "Thijs"),
+      "Thijs" <- GenCachex.get(pid, "name"),
+      :ok <- GenCachex.delete(pid, "name"),
+      nil <- GenCachex.get(pid, "name")
+    do
+      :ok
+    else
+      result when is_binary(result) ->
+        {:error, "got #{inspect result} while expecting nil"}
+      something ->
+        {:error, "unexpected, got #{something}"}
+    end
   end
 
   verify "store and wipe all" do
-    {:ok, pid} = GenCachex.start_link()
-    nil == GenCachex.get(pid, "name")
-    :ok == GenCachex.put(pid, "name", "Arno")
-    "Arno" == GenCachex.get(pid, "name")
-    :ok == GenCachex.wipe(pid)
-    nil == GenCachex.get(pid, "name") && :ok
+    with {:ok, pid} <-GenCachex.start_link(),
+      nil <- GenCachex.get(pid, "name"),
+      :ok <- GenCachex.put(pid, "name", "Arno"),
+      "Arno" <- GenCachex.get(pid, "name"),
+      :ok <- GenCachex.wipe(pid),
+      nil <- GenCachex.get(pid, "name")
+    do
+      :ok
+    else
+      result when is_binary(result) ->
+        {:error, "got #{inspect result} while expecting nil"}
+      something ->
+        {:error, "unexpected, got #{something}"}
+    end
   end
 end
